6d4d4f7ba319e32d1e16d03c149524481b6fbffb
mikalvar <mikamag96@gmail.com>
2026-09-02 18:09:22 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -297,23 +297,6 @@ return (
           </div>
         </div>
 </div>
-
-<button
-  onClick={descargarPNG}
-  style={{
-    width: "100%",
-    marginTop: "12px",
-    padding: "12px",
-    background: "#16a34a",
-    color: "white",
-    border: "none",
-    borderRadius: "12px",
-    fontWeight: "bold",
-    cursor: "pointer",
-  }}
->
-  📸 Descargar PNG
-</button>
       
         {/* PREVIEW */}
 
@@ -389,6 +372,23 @@ return (
   {" "}
   {fechaNacimiento}
 </div>
+
+<button
+  onClick={descargarPNG}
+  style={{
+    width: "100%",
+    marginTop: "12px",
+    padding: "12px",
+    background: "#16a34a",
+    color: "white",
+    border: "none",
+    borderRadius: "12px",
+    fontWeight: "bold",
+    cursor: "pointer",
+  }}
+>
+  📸 Descargar PNG
+</button>
             
             <div style={{ marginTop: "12px" }}>
               <b>WhatsApp:</b> {whatsapp}
